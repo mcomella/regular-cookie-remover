b5b5144fd32826bfa0597fc872dd7164c241ebcd
Michael Comella <michael.l.comella@gmail.com>
2018-03-24 11:55:51 -0700

    Add-on will delete data 1 week after startup.

--- src/main.js
+++ src/main.js
@@ -1,28 +1,56 @@
-console.log('lol, running!');
-
 let EXTENSION_NAME = "Regular Cookie Remover"
+let KEY_DELETION_TIME = "deletionTime";
 
-/*
-- Calculate duration ready for removal
-- Get preference for how often to delete
-- Notify delete on next startup
-- Notify deleted
-- docs
-*/
+let COOKIES_STR = "Cookies and other trackable browsing data "
 
-function hasDeletionDurationPassed() {
-    return true;
+function getDeletionTime() {
+    return browser.storage.local.get(KEY_DELETION_TIME).then((storage) => storage[KEY_DELETION_TIME]);
+}
+
+function getHasDeletionDurationPassed() {
+    return getDeletionTime().then((deletionTime) => {
+        let now = new Date();
+        return now > deletionTime;
+    });
+}
+
+function setDeletionTimeIfNotSet() {
+    return getDeletionTime().then((deletionTime) => {
+        if (!deletionTime) {
+            return setNewDeletionDuration();
+        }
+    });
+}
+
+function setNewDeletionDuration() {
+    let nextDeletion = new Date();
+    nextDeletion.setDate(nextDeletion.getDate() + 7); // TODO: user configurable.
+
+    let setArgs = {};
+    setArgs[KEY_DELETION_TIME] = nextDeletion;
+    return browser.storage.local.set(setArgs);
+}
+
+function notifyWhenDeletionReady() {
+    getDeletionTime().then((deletionTime) => {
+        let now = new Date();
+        let timeoutMillis = deletionTime - now;
+        setTimeout(() => notify(COOKIES_STR + "will be removed after browser restart."),
+                timeoutMillis);
+    });
 }
 
 function removeData() {
     browser.browsingData.remove(/* options */ {}, {
-        // cache: true,
+        cache: true,
         cookies: true,
-        // fileSystems: true, // TODO: wtf is this?
-        // indexedDB: true,
-        // localStorage: true,
-        // serverBoundCertificates: true,
-        // serviceWorkers: true
+        indexedDB: true,
+        localStorage: true,
+        serviceWorkers: true,
+
+        // Not used by Firefox: https://mail.mozilla.org/pipermail/dev-addons/2017-January/002467.html
+        // fileSystems: true,
+        // serverBoundCertificates
     });
 }
 
@@ -35,10 +63,15 @@ function notify(msg) {
 }
 
 function onStartup() {
-    if (hasDeletionDurationPassed()) {
-        removeData();
-        notify("Cookies and other trackable browsing data have been removed.");
-    }
+    setDeletionTimeIfNotSet().then(getHasDeletionDurationPassed).then((hasDeletionDurationPassed) => {
+        if (hasDeletionDurationPassed) {
+            removeData();
+            notify(COOKIES_STR + "have been removed.");
+            setNewDeletionDuration();
+        } else {
+            notifyWhenDeletionReady();
+        }
+    });
 }
 
 onStartup();
